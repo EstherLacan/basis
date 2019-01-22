e689649a097d95b20bb84d50233370c3356238f9
jiangfw <jiangfengwei_2@126.com>
2019-01-23 00:29:45 +0800

    添加jfw插件

--- src/main/java/com/jiangfw/common/lang/test/json/entity/Address.java
+++ src/main/java/com/jiangfw/common/lang/test/json/entity/Address.java
@@ -1,11 +1,14 @@
 package com.jiangfw.common.lang.test.json.entity;
 
+
 public class Address {
 
     private String a1 = "a1";
     private String a2 = "a2";
 
     /**
+     * getA1.
+     *
      * @return the a1
      */
     public String getA1() {
@@ -13,6 +16,8 @@ public class Address {
     }
 
     /**
+     * setA1.
+     *
      * @param a1 the a1 to set
      */
     public void setA1(String a1) {
@@ -20,6 +25,8 @@ public class Address {
     }
 
     /**
+     * getA2.
+     *
      * @return the a2
      */
     public String getA2() {
@@ -27,6 +34,8 @@ public class Address {
     }
 
     /**
+     * setA2.
+     *
      * @param a2 the a2 to set
      */
     public void setA2(String a2) {
